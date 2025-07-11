479fa3661bdc0a4139263e510ad34672754d3346
ido <1idokeysar@gmail.com>
2025-07-11 12:16:12 +0300

    TASK2 commit2

--- src/codes/LedController.java
+++ src/codes/LedController.java
@@ -1,30 +1,21 @@
 package src.codes;
-import java.awt.Color;
-
-import LedsSim.LedSim;
 import LedsSim.LedStrip;
 
 public class LedController {
     protected LedStrip strip;
     protected AnimationBase currentAnimation;
 
-
     public LedController(LedStrip strip){
         this.strip = strip;
     }
 
-    
-
     public void periodic(){
-        
-
-        currentAnimation.setStrip(strip);
-        currentAnimation.periodic();
-        
+        currentAnimation.periodic();     
     }
 
     public void setAnimation(AnimationBase animation){
         this.currentAnimation = animation;
+        currentAnimation.setStrip(strip);
+        currentAnimation.init();
     }
-    
 }
