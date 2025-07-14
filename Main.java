28807b29e5f514ab7f08f0a399cdff4bc3da14e0
idok <1idokeysar@gmail.com>
2025-07-14 13:55:05 +0300

    task5 commit

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import LedsSim.LedSim;
 import java.awt.Color;
 import src.codes.BlinkAnimation;
+import src.codes.BlinkAnimationTimed;
 import src.codes.LedController;
 
 public class Main {
@@ -8,8 +9,8 @@ public class Main {
 
         LedSim ledSim = LedSim.getRowsSim(100);
         LedController LC = new LedController(ledSim);
-
-        LC.setAnimation(new BlinkAnimation(new Color(100, 0, 40), 5, 10.0));
+        BlinkAnimation blinkAnimation = new BlinkAnimation(new Color(100, 0, 40), 5);
+        LC.setAnimation(new BlinkAnimationTimed(blinkAnimation, 10.0));
 
         while (true)
             LC.periodic();
